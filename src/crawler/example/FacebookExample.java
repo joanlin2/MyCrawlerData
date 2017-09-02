ff9f86a60a88e488e9c42daead983d3a1f92e11c
joan <joanlin2@yahoo.com.tw>
2017-09-02 16:39:13 +0800

    靠北系列

--- src/crawler/example/FacebookExample.java
+++ src/crawler/example/FacebookExample.java
@@ -27,7 +27,7 @@ public class FacebookExample {
 		String uri = 
 				"https://graph.facebook.com/v2.5"
 				+ "/search?q=%E9%9D%A0%E5%8C%97&type=page&limit=1000&fields=name,id,likes,talking_about_count"
-				+ "&access_token=EAACEdEose0cBAMzLyMlMxZAY73J1TBiZAHJhrF8du8fLhMmGYrUA3W9ezJDQk9oZAa07o4F2drZAL2k1cCtgdpl5LZClZCo6733A3xddNbjSAoTxEZBWY4rKnzLWTU3I0KMgT0qsw6eIRfdzg9RZCIkuTZCtGZCyzct657fywQpsFCNAZDZD";
+				+ "&access_token=EAACEdEose0cBAFdROV4TIXGhokXD5bBG3oTYee2nb1wtmhxvXaUNPhf3JaTmjjYZCZBT8ZAX68mItpnv2SSSi1rJEdkV18l9vVgtWhmaS7rOv05xUVf7LC9Uz7PiZB1X1kkGbprWmbInVlp8DgBjXaFXkYtiu1RdflFFodtZAjOu3FHvAsN0jZBC8FAZBap5Cr6tZBmmG5E3fTam7pvPfYeq";
 
 
 
