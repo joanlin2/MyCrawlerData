ed8da6ec27f6c191e1c376f459536729f76fac55
joan <joanlin2@yahoo.com.tw>
2017-09-05 16:47:04 +0800

    取得上櫃每日交易資料

--- src/stock/StockDailyOTC.java
+++ src/stock/StockDailyOTC.java
@@ -5,18 +5,18 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 
 /**
- * 練習擷取證交所提供的盤後資訊
+ * 擷取證交所提供的盤後資訊
  * 
  * 重點
- * 1. Json取得每日交易資料
+ * 1. Json取得上櫃每日交易資料
  * 
  * @author Joan lin
+ * 2017/09/05
  *
  */
 public class StockDailyOTC {
 	
 	public static void main(String[] args) {
-		String csvString ="";
 		String uri = "http://www.tpex.org.tw/web/stock/aftertrading"
 				+ "/daily_trading_info/st43_result.php?l=zh-tw"
 				+ "&d=106/09&stkno=1336";
@@ -24,34 +24,31 @@ public class StockDailyOTC {
 
 
 
-//		System.out.println(
-//			CrawlerPack.start()
-//			//	.setRemoteEncoding("big5")
-//					.getFromJson(uri)
-//				.getElementsByTag("aadata")
-//				//.select("td:matchesOwn(^[\\+\\-]?([0-9]{1,3},)*[0-9]{1,3}(\\.[0-9]+)*$)")
-////				//.select("script")
-//				.toString()
-//		);
-
 		Document jsoupDoc = CrawlerPack.start()
 				.setRemoteEncoding("big5")
 				.getFromJson(uri);
 
-		// 印出整份 XML 資料
-//		System.out.println(jsoupDoc.toString());
-//		System.out.println("--------------");
+		String stockID =jsoupDoc.select("stkno").text().toString();
+		String csvString ="";
 
-		csvString = jsoupDoc.select("stkno").text().toString();
-		System.out.println(jsoupDoc.select("aadata").size() );
-		for( Element elem: jsoupDoc.select("aadata") ){
-			System.out.print(csvString);
-			System.out.print("," + elem.getElementsByIndexLessThan(8).text().replace(",","") );
-//			for( Element arr: jsoupDoc.select("array") ) {
-////				csvString = csvString+"," + arr.text().replace(",","");
-//				System.out.print("," + arr.text().replace(",",""));
-//			}
-			System.out.println();
+		String[] YYYYMMDD ={};
+
+
+		for( Element elem: jsoupDoc.select("aadata")){
+			YYYYMMDD = elem.child(0).html().replace(",","").split("/") ;
+//			System.out.println((Integer.valueOf(YYYYMMDD[0])+1911)+YYYYMMDD[1]+YYYYMMDD[2]);
+
+			csvString = stockID
+					+","+ (Integer.valueOf(YYYYMMDD[0])+1911)+YYYYMMDD[1]+YYYYMMDD[2]
+					+","+elem.child(1).html().replace(",","")
+					+","+elem.child(2).html().replace(",","")
+					+","+elem.child(3).html().replace(",","")
+					+","+elem.child(4).html().replace(",","")
+					+","+elem.child(5).html().replace(",","")
+					+","+elem.child(6).html().replace(",","")
+					+","+elem.child(7).html().replace(",","")
+					;
+			System.out.println(csvString);
 		}
 
 
