9c0ac6aecf16f757fd81bb927dc7b419142405d8
joan <joanlin2@yahoo.com.tw>
2017-09-06 18:42:25 +0800

    取得上櫃每日交易資料

---
+++ src/stock/dateTest.java
@@ -0,0 +1,33 @@
+package stock;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+
+/**
+ * Created by Joan on 2017/9/5.
+ */
+public class dateTest {
+
+    public static void main(String[] args) {
+        Calendar startDate = new GregorianCalendar(2017, Calendar.SEPTEMBER, 1);
+        Date currentDate = new Date();
+        GregorianCalendar endDate = new GregorianCalendar();
+        endDate.setTime(currentDate);
+        int days = (int)(endDate.getTimeInMillis()-startDate.getTimeInMillis())/ (1000 * 60 * 60 * 24);
+
+        System.out.println("compareTo="+ (endDate.getTimeInMillis()-startDate.getTimeInMillis())/ (1000 * 60 * 60 * 24));
+        for(int i =0; i < days; i++){
+            startDate.add(GregorianCalendar.DATE, 1); //日期遞增
+
+            String pnum = String.format("%02d",9);
+            System.out.println(pnum);
+            System.out.println(startDate.get(Calendar.YEAR) + "/"+ (startDate.get(Calendar.MONTH )+1)+ "/"+startDate.get(Calendar.DATE));
+//            System.out.println(endDate.get(Calendar.MONTH )+1);
+//            System.out.println(endDate.get(Calendar.DATE));
+
+
+        }
+    }
+}
